3e8f6df838103cd91657af4a17eb8409d45883ef
Yarkin Reis <yarkin94@gmail.com>
2024-03-13 13:55:13 -0700

    Codes are done!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template ,redirect , session ,flash , request
+from flask import Flask, render_template, redirect, session, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -15,7 +15,36 @@ def show_survey():
 
     return render_template("survey.html",survey=survey)
 
-@app.route("/questions/<int:id>")
+
+@app.route("/go", methods=["POST"])
+def start_survey():
+
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    # get the response choice
+    choice = request.form['answer']
+
+    # add this response to the session
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/questions/<int:mid>")
 def show_question(mid):
     """Display question"""
     responses = session.get(RESPONSES_KEY)
@@ -27,11 +56,11 @@ def show_question(mid):
         flash(f"Invalid question id: {mid}")
         return redirect(f"/questions/{len(responses)}")
     
-    if (len(responses) == mid):
+    if (len(responses) == len(survey.questions)):
         return redirect("/complete")
     
     question = survey.questions[mid]
-    return render_template("questions.html", questions_num=mid)
+    return render_template("questions.html", questions_num=mid, question=question)
 
 @app.route("/complete")
 def complete():
